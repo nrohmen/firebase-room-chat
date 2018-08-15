ffbcb9789ea8c5e24680a9d93560836ec5283f0f
Nur Rohman <ennurr31@gmail.com>
2018-08-15 18:00:54 +0700

    init project

--- app/src/main/java/com/nrohmen/roomchat/networking/ServerConstants.kt
+++ app/src/main/java/com/nrohmen/roomchat/networking/ServerConstants.kt
@@ -3,5 +3,5 @@ package com.nrohmen.roomchat.networking
 object ServerConstants {
     const val URL = "https://fcm.googleapis.com/"
     const val TYPE = "Content-type:application/json"
-    const val AUTH = "Authorization: key=AAAAYBeeaQY:APA91bFJaU5j7F0oIlSMDbkNW1GqN9_W3jEhobWbxtnj7kSD0UeYGs8xCnvTNOVyL4-nwSbDuCFVS5VRYcTelOEY1m8PC6po1ZrJsjVtPDqUevKL-rKHFskvvvKQnXIyS2eb1Jh12ECEEDq_f9S6GsW5HNB59ZKYDA\n"
+    const val AUTH = "YOUR_KEY"
 }
